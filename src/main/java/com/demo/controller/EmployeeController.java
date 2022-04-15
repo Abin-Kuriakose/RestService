4c90d9063ef799bba43d210e06ab11b042d12bc2
Abin-Kuriakose <euroabin@gmail.com>
2022-04-16 00:16:00 +0530

    pom changes & controller correction

--- src/main/java/com/demo/controller/EmployeeController.java
+++ src/main/java/com/demo/controller/EmployeeController.java
@@ -3,7 +3,8 @@ package com.demo.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class EmployeeController {
 
 	@RequestMapping("/get")
